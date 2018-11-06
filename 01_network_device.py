e3b9bb101239fb443ec2ad93c7122c80dbd422bf
Adam Radford (aradford) <aradford@cisco.com>
2018-11-06 13:02:21 +1100

    added support for stacks. breaks them out on sepperate lines

--- 01_network_device.py
+++ 01_network_device.py
@@ -22,11 +22,16 @@ if __name__ == "__main__":
 
         for device in response['response']:
             uptime = "N/A" if device['upTime'] is None else device['upTime']
-            print("{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
+            if  device['serialNumber'] is not None and "," in device['serialNumber']:
+                serialPlatformList = zip(device['serialNumber'].split(","), device['platformId'].split(","))
+            else:
+                serialPlatformList = [(device['serialNumber'], device['platformId'])]
+            for (serialNumber,platformId) in serialPlatformList:
+                print("{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
                   format(device['hostname'],
                          device['managementIpAddress'],
-                         device['serialNumber'],
-                         device['platformId'],
+                         serialNumber,
+                         platformId,
                          device['softwareVersion'],
                          device['role'],uptime))
 
